9dc7aacd392a2ef171c09bdd0d110982883f0581
liammmax <99236788+liammaxp@users.noreply.github.com>
2026-09-15 21:20:33 -0600

    finish

--- src/SharedPtr.h
+++ src/SharedPtr.h
@@ -1,12 +1,18 @@
 #ifndef SHARED_PTR_HEADER
 #define SHARED_PTR_HEADER
-
+#include <cassert>
+#include <utility>
+// The following class name and funciton names are designed by ChatGPT.
 class ControlBlockBase {
 public:
-    ControlBlockBase(); // TODO: implement the default constructor.
+    ControlBlockBase(){
+        refcount = 1;
+    }; // TODO: implement the default constructor.
 
     // dtor is virtual, so that we can call derived class's dtor from a ptr to this base class.
-    virtual ~ControlBlockBase(); // TODO: implement the destructor.
+    virtual ~ControlBlockBase(){
+
+    }; // TODO: implement the destructor.
 
     // pure virtual function; must be overriden by derived classes
     virtual void* managedAddress() = 0;
@@ -17,21 +23,153 @@ public:
 
     long increment()
     {
+        assert(refcount > 0);
+        refcount++;
+        return refcount;
         // TODO: increment refcount by 1 and return result.
     }
 
     long decrement()
     {
+        assert(refcount > 0);
+        refcount--;
+        return refcount;
         // TODO: decrement refcount by 1 and return result.
     }
 
     long refCount() const
     {
+        return refcount;
         // TODO: just return the refcount.
     }
 
 private:
     // TODO: add field(s) which both control block types need to have
+    long refcount;
 };
 
+
+template <typename T>
+class ControlBlock : public ControlBlockBase {
+    private:
+        T* ptr;
+    public:
+        ControlBlock(T* ptr){
+            this->ptr = ptr;
+        }
+        ~ControlBlock(){
+            delete ptr;
+        }
+        void* managedAddress() override{
+            return ptr;
+        }
+};
+template <typename T>
+class SharedPtr{
+    private:
+        ControlBlockBase* controlBlock;
+        T* storedPtr;
+    public:
+        SharedPtr(){
+            storedPtr = nullptr;
+            controlBlock = nullptr;
+        }
+        SharedPtr(T* ptr){
+            storedPtr = ptr;
+            controlBlock = new ControlBlock<T>(ptr);
+        }
+        ~SharedPtr() {
+            if (controlBlock != nullptr) {
+                if (controlBlock->decrement() == 0) {
+                    delete controlBlock;
+                }
+            }
+        }
+        SharedPtr(const SharedPtr& other) {
+            storedPtr = other.storedPtr;
+            controlBlock = other.controlBlock;
+            if (controlBlock != nullptr) {
+                controlBlock->increment();
+            }
+        }
+        SharedPtr(SharedPtr&& other){
+            storedPtr = other.storedPtr;
+            controlBlock = other.controlBlock;
+            other.storedPtr = nullptr;
+            other.controlBlock = nullptr;
+        }
+        void swap(SharedPtr<T>& other)
+        {
+            T* tempPtr = storedPtr;
+            storedPtr = other.storedPtr;
+            other.storedPtr  = tempPtr;
+            ControlBlockBase* tempBlock = controlBlock;
+            controlBlock = other.controlBlock;
+            other.controlBlock = tempBlock;
+        }
+        SharedPtr& operator=(const SharedPtr& other)
+        {
+            SharedPtr temp(other);
+            swap(temp);
+            return *this;
+        }
+        SharedPtr& operator=(SharedPtr&& other)
+        {
+            if (this != &other) {
+                reset();
+                storedPtr = other.storedPtr;
+                controlBlock = other.controlBlock;
+                other.storedPtr = nullptr;
+                other.controlBlock = nullptr;
+            }
+            return *this;
+        }
+        T& operator*() const
+        {
+            assert(storedPtr != nullptr);
+            return *storedPtr;
+        }
+        T* operator->() const
+        {
+            assert(storedPtr != nullptr);
+            return storedPtr;
+        }
+        T* get() const
+        {
+            return storedPtr;
+        }
+        bool operator==(const SharedPtr<T>& other) const
+        {
+            return storedPtr == other.storedPtr;
+        }
+        operator bool() const
+        {
+            return storedPtr != nullptr;
+        }
+        void reset()
+        {
+            SharedPtr<T> temp;
+            swap(temp);
+        }
+
+        void reset(T* other)
+        {
+            if (storedPtr != other) {
+                SharedPtr<T> temp(other);
+                swap(temp);
+            }
+        }
+        long useCount() const
+        {
+            if (controlBlock == nullptr) {
+                return 0;
+            }
+            return controlBlock->refCount();
+        }
+};
+template <typename T, typename... Args>
+SharedPtr<T> makeSharedBasic(Args&&... args)
+{
+    return SharedPtr<T>(new T(std::forward<Args>(args)...));
+}
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,70 @@
 #include <cstdlib>
+#include <cassert>
+#include <utility>
 #include "SharedPtr.h"
 
 int main() {
+   SharedPtr<int> test;
+   assert(test.get() == nullptr);
+   assert(test.useCount() == 0);
+   assert(!test);
+   SharedPtr<int> a(new int(5));
+   assert(*a == 5);
+   assert(a.useCount() == 1);
+   assert(a);
+   SharedPtr<int> b(a);
+   assert(a == b);
+   assert(a.useCount() == 2);
+   assert(b.useCount() == 2);
+
+   SharedPtr<int> c(std::move(b));
+   assert(*c == 5);
+   assert(b.get() == nullptr);
+   assert(b.useCount() == 0);
+   assert(c.useCount() == 2);
+
+   SharedPtr<int> d(new int(10));
+   d = a;
+   assert(d == a);
+   assert(a.useCount() == 3);
+   assert(d.useCount() == 3);
+
+   SharedPtr<int> e(new int(20));
+   e = std::move(d);
+
+   assert(*e == 5);
+   assert(d.get() == nullptr);
+   assert(d.useCount() == 0);
+
+   *e = 15;
+   assert(*a == 15);
+   assert(*c == 15);
+   int* raw = a.get();
+   assert(raw != nullptr);
+   assert(*raw == 15);
+   SharedPtr<int> x(new int(1));
+   SharedPtr<int> y(new int(2));
+   x.swap(y);
+   assert(*x == 2);
+   assert(*y == 1);
+
+   SharedPtr<int> r(new int(50));
+   r.reset();
+   assert(r.get() == nullptr);
+   assert(r.useCount() == 0);
+
+   SharedPtr<int> r2(new int(60));
+   r2.reset(new int(70));
+   assert(*r2 == 70);
+   assert(r2.useCount() == 1);
+
+   int* samePtr = r2.get();
+   r2.reset(r2.get());
+   assert(r2.get() == samePtr);
+   assert(*r2 == 70);
+
+   SharedPtr<int> basic = makeSharedBasic<int>(123);
+   assert(*basic == 123);
+   assert(basic.useCount() == 1);
    return EXIT_SUCCESS;
 }
